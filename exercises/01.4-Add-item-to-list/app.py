b24efe06f1c84b3bf8ed5e351d96000fd5a61457
JuanAntonaccio <jmantonaccio@gmail.com>
2022-05-21 22:37:33 +0000

    Ejercicios listas reversas

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,12 @@
 #Remember import random function here:
+import random
+
 
 my_list = [4,5,734,43,45]
+arr=[]
+for i in range(10):
+    arr.append(random.randint(0, 100))
+
+print(arr)    
 
 #The magic is here:
