18cc9797edca297e4a570f1fec9528ec01d609b6
JuanAntonaccio <jmantonaccio@gmail.com>
2022-05-22 21:33:33 +0000

    terminados con un ejercicio sin terminar

--- exercises/15.2-Parking_lot_check/app.py
+++ exercises/15.2-Parking_lot_check/app.py
@@ -5,3 +5,20 @@ parking_state = [
 ]
 
 #Your code go here:
+def get_parking_lot():
+  dispo=0
+  ocupados=0
+  
+  for i in range(len(parking_state)):
+    for j in range(len(parking_state[i])):
+      if parking_state[i][j]==1:
+        ocupados+=1
+      elif parking_state[i][j]==0:
+        dispo+=1
+  dictio={}
+  dictio['total_slots']=ocupados+dispo
+  dictio['available_slots']=dispo
+  dictio['occupied_slots']=ocupados
+  return dictio
+
+print(get_parking_lot())         
--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,5 +1,17 @@
+def lyrics_generator(lista):
+    cont=0
+    texto=""
+    for i in lista:
+        if i==0:
+            texto=texto+"Boom "
+        else:
+            texto=texto+"Drop the base "
+            cont+=1
+            if cont==3:
+                texto=texto+"!!!Break the base!!! "
+                cont=0
 
-
+    return texto
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
 print(lyrics_generator([0,0,1,1,1,0,0,0]))
